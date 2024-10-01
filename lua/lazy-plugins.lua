15bfe72e6f6eee1ed1c52f6649610a51ee02e442
Abzsorb <Abzsorb@gmail.com>
2024-10-01 22:44:26 +0200

    Added more plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -21,7 +21,6 @@ require('lazy').setup({
   require 'kickstart/plugins/todo-comments',
   require 'kickstart/plugins/mini',
   require 'kickstart/plugins/treesitter',
-  require 'kickstart.plugins.autopairs',
   { import = 'custom.plugins' },
 }, {
   ui = {
